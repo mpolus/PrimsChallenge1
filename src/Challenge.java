0d221218edc0e3c6080bfdca2243fcfc129f5aea
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-08-05 13:36:12 -0400

    la la la la

--- src/Challenge.java
+++ src/Challenge.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Challenge {
+public class Challenge {
+    public static void main(String[] args) {
+        byte myByte = -125; //-128- 127
+        short myShort = 8;
+        int myInt = 26;
+        long myLong = 80000 - 5 * myByte + myShort + myInt;
+
+        System.out.println(myLong);
+    }
 }
